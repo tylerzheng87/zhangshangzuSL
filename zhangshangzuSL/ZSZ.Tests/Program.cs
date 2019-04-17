da5853d3330903d461170ea7d6d6ce5429bbd426
Zheng Wang <Zheng.Wang@feisystems.com>
2019-04-17 10:38:18 +0800

    Quartz.net

---
+++ zhangshangzuSL/ZSZ.Tests/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Spi;
+namespace ZSZ.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IScheduler sched = new StdSchedulerFactory().GetScheduler();
+            JobDetailImpl jdBossReport = new JobDetailImpl("jdTest", typeof(TestJob));
+            IMutableTrigger triggerBossReport = CronScheduleBuilder.DailyAtHourAndMinute(10, 12).Build();
+            triggerBossReport.Key = new TriggerKey("triggerTest");
+            sched.ScheduleJob(jdBossReport, triggerBossReport);
+            sched.Start();
+        }
+    }
+}
---
+++ zhangshangzuSL/ZSZ.Tests/TestJob.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Quartz;
+using System.Data.SqlClient;
+
+namespace ZSZ.Tests
+{
+    public class TestJob : IJob
+    {
+        //execute 执行的时候另开一个线程
+        //如果需要复杂执行任务可以写cron
+        public void Execute(IJobExecutionContext context)
+        {
+            try
+            {          
+            Console.WriteLine("任务执行啦"+DateTime.Now);
+            SqlConnection conn = new SqlConnection();
+            conn.Open();
+            Console.WriteLine("ok");
+            Console.ReadKey();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
