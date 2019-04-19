72062d2555059223eccfe69c1d2109c2116d342c
Zheng Wang <Zheng.Wang@feisystems.com>
2019-04-19 09:33:01 +0800

    Autofac与MVC结合

---
+++ zhangshangzuSL/WebApplication2/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestIService;
+
+namespace WebApplication2.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IUserService UserSerive { get; set; }
+        // GET: Default
+        public ActionResult Index()
+        {
+            bool b = UserSerive.CheckLogin("abc", "213");
+            return Content(b.ToString());
+        }
+    }
+}
---
+++ zhangshangzuSL/WebApplication2/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication2
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start() 
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly).PropertiesAutowired();
+            Assembly asmService = Assembly.Load("TestService");
+            builder.RegisterAssemblyTypes(asmService).Where(t=>!t.IsAbstract).AsImplementedInterfaces();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
