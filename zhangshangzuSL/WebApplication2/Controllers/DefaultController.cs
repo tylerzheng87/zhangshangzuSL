39641f7e73381249923e57808ac0bbb492f194ed
Zheng Wang <Zheng.Wang@feisystems.com>
2019-04-25 17:04:21 +0800

    3-使用newtonjson解决Json

--- zhangshangzuSL/WebApplication2/Controllers/DefaultController.cs
+++ zhangshangzuSL/WebApplication2/Controllers/DefaultController.cs
@@ -16,5 +16,25 @@ namespace WebApplication2.Controllers
             bool b = UserSerive.CheckLogin("abc", "213");
             return Content(b.ToString());
         }
+
+        public ActionResult Test()
+        {
+            Dog dog = new Dog() {BirthDate=DateTime.Now,Id=5,Name="TMAN" };
+            return Json(dog);
+        }
+        [HttpGet]
+        public ActionResult TestJson()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult TestJson(FormCollection fc)
+        {
+            Dog dog = new Dog() { BirthDate = DateTime.Now, Id = 5 };
+            //  return Json(dog);
+            return new JsonNetResult() { Data = dog };
+            
+        }
+
     }
 }
